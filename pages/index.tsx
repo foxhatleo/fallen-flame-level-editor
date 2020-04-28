7b6464692844904992ec1c9f7c1a511662d2834b
Leo Liang <36321327+foxhatleo@users.noreply.github.com>
2020-04-28 15:28:30 +0800

    Redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,10 @@ import levelStore from "redux/LevelStore";
  * Homepage.
  */
 const Home: React.FunctionComponent = () => {
+    if (typeof window !== "undefined" && window.location.href.includes("now.sh")) {
+        window.location.replace("https://ffle.leoliang.com/");
+        return <>Redirecting... If you are not redirected within 5 seconds, click <a href={"https://ffle.leoliang.com/"}>here</a></>;
+    }
     return (
         <Provider store={levelStore}>
             <App />
